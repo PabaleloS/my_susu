7fcb3888c28e721b662db4fabd1567c6768ccdf2
Pabalelo Sethiba <pabalelosethiba.ps@gmail.com>
2024-03-17 19:12:18 +0200

    Seed update

--- app/controllers/deposits_controller.rb
+++ app/controllers/deposits_controller.rb
@@ -18,37 +18,19 @@ class DepositsController < ApplicationController
   end
 
   def create
-    # @susu = Susu.find(params[:susu_id])
-    # # @deposit = Deposit.new(deposit_params)--
+    @susu = Susu.find(params[:susu_id])
     # @deposit = @susu.deposits.new(deposit_params)
-    # member = @susu.members.find_by(user_id: current_user.id)
-    # # member = Member.find_by(susu: @susu, user_id: current_user.id)--
-    # @deposit.member = member
-    # # @deposit.susu = @susu--
-    # @deposit.date = Date.today
-    # @susu.balance += @deposit.agree_amount
-    # if @deposit.save! && @susu.save!
-    #   redirect_to susu_deposit_path(@susu, @deposit), notice: "Deposit was successfully added."
-    # end
-    @susu_id = params[:susu_id]  # Retrieve susu_id from params
-    @susu = Susu.find(@susu_id)  # Find susu based on retrieved id
-
+    # @deposit = Deposit.new(deposit_params)
     @deposit = @susu.deposits.new(deposit_params)
     member = @susu.members.find_by(user_id: current_user.id)
-    if member
-      @deposit.member = member
-      @deposit.date = Date.today
-      if @deposit.save
-        @susu.update_attribute(:balance, @susu.balance + @deposit.agree_amount)  # Update balance separately
-        redirect_to susu_deposit_path(@susu, @deposit), notice: "Deposit was successfully added."
-      else
-        render :new
-      end
-    else
-      # Handle case where user is not a member of the susu
-      redirect_to susu_path(@susu), alert: "You are not a member of this Susu."
+    # member = Member.find_by(susu: @susu, user_id: current_user.id)
+    @deposit.member = member
+    # @deposit.susu = @susu
+    @deposit.date = Date.today
+    @susu.balance += @deposit.agree_amount
+    if @deposit.save! && @susu.save!
+      redirect_to susu_deposit_path(@susu, @deposit), notice: "Deposit was successfully added."
     end
-
   end
 
   private
