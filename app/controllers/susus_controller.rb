25b72d6fafb7f9831ad9dffa0d3e388a4ddad698
Christopher Jones <joneschristopher379@gmail.com>
2024-03-19 07:04:54 +0200

    update to payment

--- app/controllers/susus_controller.rb
+++ app/controllers/susus_controller.rb
@@ -1,6 +1,24 @@
 class SususController < ApplicationController
 
   def index
+    @user = current_user
+    if params[:id].present?
+      @member = Member.find(params[:id])
+      @members = @user.members
+      @pending_susus = Susu.joins(:members).where(members: { user_id: current_user, status: 'pending' })
+      @accepted_susus = Susu.joins(:members).where(members: { user_id: current_user, status: 'accepted' })
+      @declined_susus = Susu.joins(:members).where(members: { user_id: current_user, status: 'declined' })
+    else
+      @member = nil
+      @members = @user.members
+      @pending_susus = []
+      @accepted_susus = []
+      @declined_susus = []
+    end
+  end
+
+
+  def index
       @user = current_user
       @member = Member.find(params[:id])
       @members = @user.members
@@ -9,6 +27,17 @@ class SususController < ApplicationController
       @accepted_susus = Susu.joins(:members).where(members: { user_id: current_user, status: 'accepted' })
       @declined_susus = Susu.joins(:members).where(members: { user_id: current_user, status: 'declined' })
   end
+
+
+
+
+
+
+
+
+
+
+
 
   def show
     @susu = Susu.find(params[:id])
