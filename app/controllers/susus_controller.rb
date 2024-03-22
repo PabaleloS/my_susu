3f436e783917a190b01425786cfed299eee5353d
Christopher Jones <joneschristopher379@gmail.com>
2024-03-22 06:03:21 +0200

    fix minor pages issue

--- app/controllers/susus_controller.rb
+++ app/controllers/susus_controller.rb
@@ -39,7 +39,7 @@ class SususController < ApplicationController
 
   def disburse
     susu = Susu.find(params[:id])
-    member = susu.next_member
+    member, more_members_awaiting = susu.next_member
 
     # Update balances
     ActiveRecord::Base.transaction do
@@ -50,33 +50,48 @@ class SususController < ApplicationController
 
       # Reset Susu balance
       susu.update(balance: 0)
+      # raise
+
+      # Set disbursement status based on whether more members are awaiting disbursement
+      @disbursement_status = more_members_awaiting ? :expecting_payment : :has_received_payment
+
 
       # Redirect with notice
       redirect_to susu_path(susu), notice: "#{susu.agree_amount} disbursed to #{member.user.first_name}. #{member.user.first_name}'s new balance is #{member.user.balance}."
+
     rescue ActiveRecord::RecordInvalid => e
       # Handle transaction failure
+      @disbursement_status = :disbursement_failed
       flash.now[:alert] = "Disbursement unsuccessful, please try again. Error: #{e.message}"
       render :show
     end
   end
+
+
+
   # def disburse
   #   susu = Susu.find(params[:id])
   #   member = susu.next_member
-  #   member.balance += susu.balance
-  #   member.user.balance += member.balance
-  #   member.save
 
-  #   member.user.save
+  #   # Update balances
+  #   ActiveRecord::Base.transaction do
+  #     member.update(balance: member.balance + susu.balance)
+  #     puts "Member balance after update: #{member.balance}" # Debug output
+  #     member.user.update(balance: member.user.balance + susu.balance)
+  #     puts "User balance after update: #{member.user.balance}" # Debug output
 
-  #   susu.balance = 0
+  #     # Reset Susu balance
+  #     susu.update(balance: 0)
 
-  #   if susu.save && member.save && member.user.save
-  #     redirect_to susu_path(susu), notice: "#{susu.agree_amount} disbursed to #{member.user.first_name}"
-  #     flash.now[:alert] = "#{member.user.first_name}'s new balance is #{member.user.balance}"
-  #   else
-  #     render :show, alert: "Disbursement unsuccessful, please try again"
+  #     # Redirect with notice
+  #     redirect_to susu_path(susu), notice: "#{susu.agree_amount} disbursed to #{member.user.first_name}. #{member.user.first_name}'s new balance is #{member.user.balance}."
+  #   rescue ActiveRecord::RecordInvalid => e
+  #     # Handle transaction failure
+  #     flash.now[:alert] = "Disbursement unsuccessful, please try again. Error: #{e.message}"
+  #     render :show
   #   end
   # end
+
 
   def accept_invite
     @user = current_user
